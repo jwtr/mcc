db13f81bc5db00676049ecc5fedadbe0ed48bf61
jwtr <59117794+jwtr@users.noreply.github.com>
2021-05-31 15:51:50 +0100

    Extract Header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
+import Header from './components/Header';
 import HeroStats from './components/HeroStats';
 import VillainStats from './components/VillainStats';
 
 function App() {
   return (
     <div className="flex flex-col h-screen text-white bg-gray-700">
-      <header className="p-5 bg-gray-700 text-center text-5xl font-display">
-        Marvel Champions Companion
-      </header>
+      <Header />
       <main className="flex flex-wrap overflow-y-auto p-5 text-center">
         <HeroStats />
         <VillainStats />
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="p-5 bg-gray-700 text-center text-5xl font-display">
+      Marvel Champions Companion
+    </header>
+  );
+}
+
+export default Header;
